aee667dc5a365d5a3429ff95487adc6351e1e752
Kuddleman <edl1155@gmail.com>
2022-01-27 19:30:45 -0800

    add load indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native'
 import { Card, ListItem } from 'react-native-elements'
 import { connect } from 'react-redux'
 import { baseUrl } from '../shared/baseUrl'
+import Loading from './loadingComponent'
 
 const mapStateToProps = state => {
   return {
@@ -33,6 +34,27 @@ class About extends Component {
           subtitle={item.description}
           leftAvatar={{ source: { uri: baseUrl + item.image } }}
         />
+      )
+    }
+
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      )
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
       )
     }
 
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { FlatList } from 'react-native'
+import { View, FlatList } from 'react-native'
 import { Tile } from 'react-native-elements'
 import { connect } from 'react-redux'
 import { baseUrl } from '../shared/baseUrl'
+import Loading from './loadingComponent'
 
 const mapStateToProps = state => {
   return {
@@ -27,6 +28,17 @@ class Directory extends Component {
           onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
           imageSrc={{ uri: baseUrl + item.image }}
         />
+      )
+    }
+
+    if (this.props.campsites.isLoading) {
+      return <Loading />
+    }
+    if (this.props.campsites.errMess) {
+      return (
+        <View>
+          <Text>{props.campsites.errMess}</Text>
+        </View>
       )
     }
 
