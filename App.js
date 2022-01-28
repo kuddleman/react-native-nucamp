91fa8a32e029b794041b0b7023b0cafc8b079fd9
Kuddleman <edl1155@gmail.com>
2022-01-27 18:56:31 -0800

    connect app to redux

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
 import Main from './components/MainComponent'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { ConfigureStore } from './redux/configureStore'
+
+const store = ConfigureStore()
 
 export default function App() {
-  return <Main />
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  )
 }
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.1.243/'
+export const baseUrl = 'http://192.168.1.243:3001/'
