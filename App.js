1178a09985592e3b7e7a85bac1c3cd551bcb372d
Kuddleman <edl1155@gmail.com>
2022-02-02 18:38:30 -0800

    complete Persist Redux Store exercise

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import { StatusBar } from 'expo-status-bar'
 import Main from './components/MainComponent'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { ConfigureStore } from './redux/configureStore'
+import { PersistGate } from 'redux-persist/integration/react'
+import Loading from './components/loadingComponent'
 
-const store = ConfigureStore()
+const { persistor, store } = ConfigureStore()
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate loading={loading} persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
   )
 }
